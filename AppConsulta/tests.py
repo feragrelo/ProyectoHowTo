438d404552149dc5987a8e3cf91afa0234ea592c
Fernando Agrelo <feragrelo96@gmail.com>
2022-11-06 19:59:13 -0300

    Proyecto Final

--- AppConsulta/tests.py
+++ AppConsulta/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from AppConsulta.models import Consulta
+from django.contrib.auth.models import User
 
-# Create your tests here.
+class ViewTestCase(TestCase):
+    
+    def test_crear_consulta(self):
+        user=User.objects.create(username="Carlos", password="admin123",email="carlos@gmail.com")
+        consulta= Consulta.objects.create(user=user,contenido="por qué al ingresar.....",telefono="1651616",email="carlos@gmail.com")
+        
+        todos_los_user= User.objects.all()
+        todas_las_consultas = Consulta.objects.all()
+        assert len(todas_las_consultas) == 1
+        assert todas_las_consultas[0].contenido == "por qué al ingresar....."
--- AppHowTo/tests.py
+++ AppHowTo/tests.py
@@ -1,11 +1,24 @@
 from django.test import TestCase
-from AppHowTo.models import Articulo
+from AppHowTo.models import Articulo,Autor
 
 
 class ViewTestCase(TestCase):
+    
+    def test_crear_autor(self):
+        autor1= Autor.objects.create(nombre="Pablo",apellido="Vegetti",email="pv@gmail.com")
+        autor2= Autor.objects.create(nombre="Miguel",apellido="Suarez",email="mg@gmail.com")
+        todos_los_autores = Autor.objects.all()
+        assert len(todos_los_autores) == 2
+        assert autor1.nombre == "Pablo"
+        assert autor2.email == "mg@gmail.com"
 
     def test_crear_articulo(self):
-        Articulo.objects.create(nombre="Belgrano campeón", contenido="Belgrano es locura belgrano es pasión",autor="Vegetti")
+        autor= Autor.objects.create(nombre="Pablo",apellido="Vegetti",email="pv@gmail.com")
+        Articulo.objects.create(nombre="Belgrano campeón", contenido="Belgrano es locura belgrano es pasión",autor=autor)
         todos_los_articulos = Articulo.objects.all()
+        todos_los_autores = Autor.objects.all()
         assert len(todos_los_articulos) == 1
         assert todos_los_articulos[0].contenido == "Belgrano es locura belgrano es pasión"
+        
+    
+    
